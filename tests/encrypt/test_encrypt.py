ffdc912b5c5aaf1ad53ae08e92a13b70dfb74c25
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-09-20 14:44:32 -0300

    Feat: Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(key="A", message="abc")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(key=1, message=2)
+
+    assert encrypt_message(key=100, message="abc") == "cba"
+    assert encrypt_message(key=3, message="pessego") == "sep_oges"
+    assert encrypt_message(key=4, message="pessego") == "oge_ssep"
