2d50a6e621aa5afc07004ae489ea4e9c13437998
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-09-20 15:41:26 -0300

    Feat: Requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,17 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    array = []
+
+    for num in nums:
+
+        if type(num) != int or num < 0:
+            return False
+
+        # if nums.count(num) > 1:
+        #     return num
+
+        if num in array:
+            return num
+
+        array.append(num)
+
+    return False
