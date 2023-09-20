ee2abf76c8b3d3760bfeb95700a527799c19cc3b
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-09-20 16:20:42 -0300

    Fix: Requisito 5 foi refatorado utilizando set. Por algum motivo não foi possivel realizar o requisito com método count

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,17 +1,13 @@
-from collections import defaultdict
-
-
 def find_duplicate(nums):
-    array = defaultdict(lambda: 0)
-
+    array = set()
     for num in nums:
 
         if type(num) != int or num < 0:
             return False
 
-        array[num] += 1
-
-        if array[num] > 1:
+        if num in array:
             return num
+
+        array.add(num)
 
     return False
